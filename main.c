c405ff44f0b670370a0524cb8a2245cb86c4ec8e
Epiano28K <nickwu0301@gmail.com>
2018-07-20 19:12:01 +0800

    [DAVID] Submission 32/69

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int function()
+int rob(int* nums, int numsSize)
 {
+    int i=0,j=1,m=0,m1=0;
+    while(i<numsSize){
+        m=m+nums[i];
+        i=i+2;
+        m1=m1+nums[j];
+        j=j+2;
+    }
+    if(m>m1){
+        return m;
+    }
+    else
+    return m1;
 }
 
 int main(void)
